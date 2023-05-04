0492eb7ea45567a2cc73edf13196677fbaf3b61a
Ayana Alenova <alenova.ayana@gmail.com>
2023-05-04 10:19:34 +0400

    created categories

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import Categories from './components/Categories';
 import Header from './components/Header';
-import './scss/app.scss'
+import './scss/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className='wrapper'>
-        <Header/>
-
+      <div className="wrapper">
+        <Header />
+        <div class="content">
+          <div class="container">
+            <div class="content__top">
+              <Categories/>
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
